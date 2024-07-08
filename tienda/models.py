e25561fcd410397b4136b53fd2e55d5b1cdb7331
Criss-TeC <cristiantelloc@gmail.com>
2024-07-08 19:12:45 -0400

    #

--- tienda/models.py
+++ tienda/models.py
@@ -12,11 +12,7 @@ class Producto(models.Model):
         return self.nombre
 
 class Venta(models.Model):
-<<<<<<< HEAD
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    cantidad_vendida = models.IntegerField()
-=======
->>>>>>> 6c037febb7494d48f150876fe8eecd596227bf87
+
     fecha = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -29,18 +25,9 @@ class VentaDetalle(models.Model):
     subtotal = models.DecimalField(max_digits=10, decimal_places=2)
 
     def save(self, *args, **kwargs):
-<<<<<<< HEAD
-        if not self.pk:  # Solo se ejecuta al crear una nueva venta
-            self.producto.cantidad -= self.cantidad_vendida
-            self.producto.save()
-        super(Venta, self).save(*args, **kwargs)
 
-    def __str__(self):
-        return f"Venta de {self.cantidad_vendida} {self.producto.nombre}"
-=======
         self.subtotal = self.producto.precio * self.cantidad
         super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.producto.nombre} - {self.cantidad}'
->>>>>>> 6c037febb7494d48f150876fe8eecd596227bf87
